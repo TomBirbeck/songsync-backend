5eb9839810ef6d899bb949fb4a7cb0eb90526e13
unknown <tombirbeck47@gmail.com>
2023-05-31 22:41:26 +0100

    added crud handler functions

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,19 +1,31 @@
-export const addSong = (song, artist, lyrics) => {
+import pool from '../db/index.js'
+
+export const addSong = async (song, artist, lyrics) => {
     //add song to database
+    const res = await pool.query('INSERT INTO songs (name, artist, lyrics) VALUES (($1), ($2), ($3)) RETURNING * :', [song, artist, lyrics]);
+    return res.rows;
 }
 
-export const getSongById = (id) => {
+export const getSongById = async (id) => {
     //get song from db using id
+    const res = await pool.query('SELECT * FROM songs WHERE id = ($1);', [id]);
+    return res.rows;
 }
 
-export const getAllSongs = () => {
+export const getAllSongs = async () => {
     // get all songs from db
+    const res = await pool.query('SELECT * from songs;');
+    return res.rows;
 }
 
-export const editSong = (id, song, artist, lyrics) => {
+export const editSong = async (id, song, artist, lyrics) => {
     // patch song
+    const res = await pool.query('UPDATE songs SET name = ($1), artist = ($2), lyrics = ($3) WHERE id = ($4) RETURNING *:', [song, artist, lyrics, id]);
+    return res.rows;
 }
 
-export const deleteSong = (id) => {
+export const deleteSong = async (id) => {
     // delete song using id
+    const res = await pool.query('DELETE FROM songs WHERE id = ($1);', [id]);
+    return res.rows;
 }
