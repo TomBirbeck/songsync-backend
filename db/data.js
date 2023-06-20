4c9fa9513e837412e87cfa7cc25f1d7f2d8a5216
unknown <tombirbeck47@gmail.com>
2023-06-20 22:51:04 +0100

    added extra seed data

--- db/data.js
+++ db/data.js
@@ -60,6 +60,137 @@ const songList = [
                 Just a-lettin' ya know I'm in total control,
                 Oh yes many people know that I got good taste,`
       },
+      {
+        artist: 'Backstreet Boys',
+        song: 'I Want it That Way',
+        lyrics: `Yeah-eh-heah,
+                You are my fire,
+                The one desire,
+                Believe when I say,
+                I want it that way,
+                But we are two worlds apart,`
+      },
+      {
+        artist: 'Scissor Sisters',
+        song: 'Take Your Mama',
+        lyrics: `When you grow up,
+                 livin' like a good boy oughta,
+                And your mama takes a shine to her best son,
+                Something different,
+                all the girls they seem to like you,
+                Cause you're handsome,`
+      },
+      {
+        artist: 'Johnny Cash',
+        song: 'Ring of Fire',
+        lyrics: `Love is a burnin' thing,
+                  And it makes a fiery ring,
+                  Bound by wild desire,
+                  I fell into a ring of fire,
+                  I fell into a burnin' ring of fire,
+                  I went down down down,`
+      },
+      {
+        artist: 'David Bowie',
+        song: 'Rebel Rebel',
+        lyrics: `Doo doo doo-doo doo doo doo doo,
+                  Doo doo doo-doo doo doo doo doo,
+                  Doo doo doo-doo doo doo doo doo,
+                  You've got your mother in a whirl,
+                  She's not sure if you're a boy or a girl,
+                  Hey babe your hair's alright,`
+      },
+      {
+        artist: 'Twister Sister',
+        song: `We're Not Gonna Take It`,
+        lyrics: `Oh we're not gonna take it,
+                  No, we ain't gonna take it,
+                  Oh we're not gonna take it anymore,
+                  We've got the right to choose it,
+                  There ain't no way we'll lost it,
+                  This is our life this is our song,`
+      },
+      {
+        artist: 'Olivia Rodrigo',
+        song: 'Drivers Licence',
+        lyrics: `I got my driver's license last week,
+                  Just like we always talked about,
+                  'Cause you were so excited for me,
+                  To finally drive up to your house,
+                  But today I drove through the suburbs,
+                  Crying 'cause you weren't around,
+        `
+      },
+      {
+        artist: 'The Doors',
+        song: 'Light My Fire',
+        lyrics: `You know that it would be untrue,
+                  You know that I would be a liar,
+                  If I was to say to you,
+                  Girl we couldn't get much higher,
+                  Come on baby, light my fire,
+                  Come on baby, light my fire,`
+      },
+      {
+        artist: 'Eminem ft Beyonce',
+        song: 'Walk on Water',
+        lyrics: `I walk on water,
+                  But I ain't no Jesus,
+                  I walk on water,
+                  But only when it freezes,
+                  Why are expectations so high?,
+                  Is it the bar I set?,`
+      },
+      {
+        artist: 'Lewis Capaldi',
+        song: 'Somebody to Love',
+        lyrics: `I'm going under and this time I fear there's no one to save me,
+                  This all or nothing really got a way of driving me crazy,
+                  I need somebody to heal,
+                  Somebody to know,
+                  Somebody to have,
+                  Somebody to hold,`
+      },
+      {
+        artist: 'The Libertines',
+        song: `Can't Stand me now`,
+        lyrics: `An ending fitting for the start,
+                  You twist and tore our love apart, 
+                  Your light fingers threw the dark,
+                  That shattered the lamp and into darkness it cast us,  
+                  No, you've got it the wrong way round,
+                  You shut me up, and blamed it on the brown,`
+      },
+      {
+        artist: 'Arctic Monkeys',
+        song: 'Do I Wanna Know?',
+        lyrics: `Have you got color in your cheeks,
+                  Do you ever get the feeling that you can't shift the tide,
+                  That sticks around like something's in your teeth,
+                  And some aces up your sleeve,
+                  I had no idea that you're in deep,
+                  I dreamt about you near me every night this week,`
+      },
+      {
+        artist: 'Bonnie Tyler',
+        song: 'Holding Out for a Hero',
+        lyrics: `Where have all the good men gone,
+                  And where are all the gods?,
+                  Where's the streetwise Hercules to fight the rising odds?,
+                  Isn't there a white knight upon a fiery steed?,
+                  Late at night I toss and I turn,
+                  And I dream of what I need,`
+      },
+      {
+        artist: 'Tina Turner',
+        song: `We Don't Need Another Hero`,
+        lyrics: `Out of the ruins,
+                  Out from the wreckage, 
+                  Can't make the same mistake this time, 
+                  We are the children,
+                  The last generation, 
+                  We are the ones they left behind,`
+      },
     ]
 
     export default songList
